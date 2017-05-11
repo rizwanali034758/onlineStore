7702fb18e6aafe10a2710a680474c323df1f6b12
Rizwan Ali <rizwanali034758@gmail.com>
2017-05-11 21:18:02 +0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cartpk</title>
+	<style>
+		.header{
+			
+		height:30px;
+		width:1366px;
+		background-color:lightgrey;
+		margin-left:-8px;
+		margin-top:-16px;
+		}
+		.headermenu ul li{
+			
+			display:inline;
+			float:right;
+			margin-right:25px;
+			padding-top:7px;
+			
+			font-size:14px;
+			text-decoration:none;
+		}
+		a{
+			text-decoration:none;
+			
+		}
+		a:hover{
+			color:red;
+		}
+	</style>
+</head>
+<body>
+	<div class="header">
+		<div class="headermenu">
+			<ul>
+				
+					<li><a href="#" title="Login">Login</a></li>
+					<li>|</li>
+					<li><a href="#" title="Check Out">Check Out</a></li>
+					<li>|</li>
+					<li><a href="#" title="My Cart">My Cart</a></li>
+					<li>|</li>
+					<li><a href="#" title="Account">Account</a></li>
+					
+				<ul>
+			</table>
+		</div>
+	
+	</div>
+
+</body>
+</html>
